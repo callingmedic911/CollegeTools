2649d8ec3f39923bd12782b341c09b2bc5604eeb
Aditya Pandey <aditya@autospace.co>
2015-03-23 18:14:57 +0530

    Initial commit

---
+++ app/src/main/java/com/adityaworks/collegetools/BunkCalculator.java
@@ -0,0 +1,87 @@
+package com.adityaworks.collegetools;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class BunkCalculator extends ActionBarActivity {
+
+    private DrawerLayout drawerLayout;
+    private ListView listView;
+
+    private String[] navigationDrawerItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bunk_calculator);
+
+        navigationDrawerItems = getResources().getStringArray(R.array.navigation_drawer_items);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        listView = (ListView) findViewById(R.id.left_drawer);
+
+        // set up the drawer's list view with items and click listener
+        listView.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, navigationDrawerItems));
+        listView.setOnItemClickListener(new DrawerItemClickListener());
+
+        if (savedInstanceState == null) {
+            selectItem(1);
+        }
+    }
+
+    private void selectItem(int position) {
+        // update the main content by replacing fragments
+        FragmentTransaction fragmentManager = getSupportFragmentManager().beginTransaction();
+
+        switch(position) {
+            default:
+            case 1:
+                fragmentManager.replace(R.id.content_frame, new BunkCalculatorFragment());
+                break;
+        }
+
+        fragmentManager.commit();
+
+        // update selected item and title, then close the drawer
+        listView.setItemChecked(position, true);
+        drawerLayout.closeDrawer(listView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_bunk_calculator, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /* The click listener for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+}
